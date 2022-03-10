2715317f45049284c3fa33567a8bd6d603f36946
Nelson Donfack Tedonjio <P2559549@my365.dmu.ac.uk>
2022-03-10 23:11:17 +0000

    other update

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -8,5 +8,8 @@ namespace ClassLibrary
         public DateTime PurchaseTime { get; set; }
         public string DeliveryAddress { get; set; }
         public bool Delivery { get; set; }
+        public Int32 ItemQuantity { get; set; }
+        public string Notes { get; set; }
+        public Guid OrderCode { get; set; }
     }
 }
