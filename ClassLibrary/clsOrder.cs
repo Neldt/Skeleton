c976f013f5a526aeab6a698cdfa75f82737639f6
Nelson Donfack Tedonjio <P2559549@my365.dmu.ac.uk>
2022-03-10 19:06:36 +0000

    my new changeson the 10/03/22

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,6 +1,12 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
+        public bool Active { get; set; }
+        public DateTime PurchaseTime { get; set; }
+        public string DeliveryAddress { get; set; }
+        public bool Delivery { get; set; }
     }
 }
