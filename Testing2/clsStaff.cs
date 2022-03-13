85d93afe9df23daa176af54616c9f8e2d78358f1
Chris <Chris@LAPTOP-6HA3ISB4>
2022-03-13 15:18:53 +0000

    StaffManagement - update

---
+++ Testing2/clsStaff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StaffTesting
+{
+    internal class clsStaff
+    {
+        public string StaffFullName { get; internal set; }
+        public int StaffID { get; internal set; }
+        public bool Availability { get; internal set; }
+        public string StaffRole { get; internal set; }
+        public DateTime StartDate { get; internal set; }
+        public int CustomerID { get; internal set; }
+    }
+}
