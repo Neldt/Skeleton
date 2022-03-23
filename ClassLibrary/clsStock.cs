064ec36a69d2f43fa740c0769f6a65cad64190dc
Adnaan Khan <adnaan.khan15@gmail.com>
2022-03-23 13:25:20 +0000

    Done Middle Layer

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Available { get; set; }
+        public int ItemNo { get; set; }
+        public string ItemDescription { get; set; }
+        public int ItemQuantity { get; set; }
+        public double Price { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }
--- Testing3/tstStock.cs
+++ Testing3/tstStock.cs
@@ -15,5 +15,92 @@ namespace Testing3
             //test to see that it exists
             Assert.IsNotNull(AnStock);
         }
+
+        [TestMethod]
+        public void AvailablePropertyOK()
+        {
+            //create an instance of the class we want to create 
+            clsStock AnStock = new clsStock();
+            //create some test data
+            Boolean TestData = true;
+            //assign the data to the property
+            AnStock.Available = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AnStock.Available, TestData);
+        }
+
+        [TestMethod]
+        public void ItemNoPropertyOK()
+        {
+
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //create some test data
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnStock.ItemNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStock.ItemNo, TestData);
+
+        }
+
+        [TestMethod]
+        public void ItemDescriptionPropertyOk()
+        {
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //create some test data to assign to the property
+            string TestData = "LE1 4AB";
+            //assign the data to the property
+            AnStock.ItemDescription = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStock.ItemDescription, TestData);
+        }
+
+        [TestMethod]
+        public void ItemQuantityOK()
+
+        {
+
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //create some test data
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnStock.ItemQuantity = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStock.ItemQuantity, TestData);
+
+        }
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnStock.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStock.DateAdded, TestData);
+
+        }
+        [TestMethod]
+        public void PricePropertyOk()
+        {
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //create some test data to assign to the property
+            double TestData = 6.9;
+            //assign the data to the property
+            AnStock.Price = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStock.Price, TestData);
+
+        }
+        
+
+
     }
+
 }
